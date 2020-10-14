5cefc8beb7b61821213ad02316f30cd71335cd44
Kadu Floresta <cefloresta1@gmail.com>
2020-10-13 22:50:39 -0300

    13/09/2020

--- exercicios/exer6/js/jsSeis.js
+++ exercicios/exer6/js/jsSeis.js
@@ -1,15 +1,36 @@
 // Função
 $(function () {
-    // Definir a Animação (Direita e Esquerda)
-    // ...para a classe
-    $(".bg-dark").mouseenter(function () {
-        //Animação para a direita += a 50 pixels
+    // Animação do Elemento com eventos do mouse
+    // ...para a id "elementoEsquerda"
+    $("#elementoEsquerda").mouseenter(function () {
+        // Quando o mouse entra no elemento em questão
+        // ...ele se desloca para a direita em 50px.
+        $(this).animate({
+            left: '+=50px'
+        });
+    });
+    $("#elementoEsquerda").mouseleave(function () {
+        // Quando o mouse sai do elemento em questão
+        // ...ele se desloca para a esquerda em 50px.
+        // (Posição Original)
+        $(this).animate({
+            left: '-=50px'
+        });
+    });
+
+    // Animação do Elemento com eventos do mouse
+    // ...para a id "elementoDireita"
+    $("#elementoDireita").mouseenter(function () {// Quando o mouse sai do elemento em questão
+        // Quando o mouse entra no elemento em questão
+        // ...ele se desloca para a esquerda em 50px.
         $(this).animate({
             right: '+=50px'
         });
     });
-
-    $(".bg-dark").mouseleave(function () {
+    $("#elementoDireita").mouseleave(function () {
+        // Quando o mouse sai do elemento em questão
+        // ...ele se desloca para a direita em 50px.
+        // (Posição Original)
         $(this).animate({
             right: '-=50px'
         });
